5907f3a63e676981489f75090cb6e57ea04f4919
ImSreyas <sreyassatheesh755@gmail.com>
2024-07-12 11:26:20 +0530

    initial commit

---
+++ vlc/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=~/.config/vlc/vlcrc # Path to the vlc config
+PATTERN="stereo-mode" # Pattern that should be replaced
+REPLACEMENT="stereo-mode=1" # Text that will replace the pattern
+
+# Check if the file exists
+if [ ! -f "$FILE" ]; then
+    echo "File not found: $FILE"
+    exit 1
+fi
+
+sed -i "/$PATTERN/c\\$REPLACEMENT" "$FILE"
+
+echo "Success..."
+
